31358fd66e6a5552090c99cb614c3460050f71c4
riteshdawale02 <riteshdawale1596@gmail.com>
2025-08-04 20:08:22 +0530

    Building Todo list API

---
+++ src/main/java/com/example/todo_list_demo/TodoListDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_list_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todo_list_demo/TodoListDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_list_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
